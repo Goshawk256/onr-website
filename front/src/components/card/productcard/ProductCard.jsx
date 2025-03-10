6b4b4146b9ec2c1588ce185c5a1065f59c769a8e
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-10 20:48:55 +0300

    Enhance home page layout by adding ProductCard components and improving CSS for better structure and responsiveness

--- front/src/components/card/productcard/ProductCard.jsx
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -3,7 +3,7 @@ import "./ProductCard.css";
 function ProductCard() {
   return (
     <div className="product-card-main">
-      <div className="product-card-content"></div>
+      <div className="product-card-content">selam bu bir ürün.</div>
     </div>
   );
 }
--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Home.css";
 import Header from "../../components/header/Header";
+import ProductCard from "../../components/card/productcard/ProductCard";
 function Home() {
   return (
     <div className="home-main">
       <Header />
-      <div className="home-content"></div>
+      <div className="home-content">
+        <div className="slider-area"></div>
+        <div className="product-area">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
     </div>
   );
 }
