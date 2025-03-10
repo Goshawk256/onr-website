8931e3353c14f0826d1fae2a7b6b5d31a5ea4d6a
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-10 20:32:37 +0300

    Add ProductCard component with initial structure and styling

---
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ProductCard.css";
+function ProductCard() {
+  return (
+    <div className="product-card-main">
+      <div className="product-card-content"></div>
+    </div>
+  );
+}
+
+export default ProductCard;
