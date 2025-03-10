b775f9bcfd5a6598ed257762a06d483524ddd024
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-10 21:50:29 +0300

    Add image imports for product display on home page

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./Home.css";
 import Header from "../../components/header/Header";
 import ProductCard from "../../components/card/productcard/ProductCard";
+import img1 from "../../assets/saat.jpeg";
+import img2 from "../../assets/bayi.png";
+import img3 from "../../assets/bayi2.png";
 function Home() {
   return (
     <div className="home-main">
