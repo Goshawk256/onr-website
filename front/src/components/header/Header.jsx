17ef040e08799128693fee9276fb3bc51007aa29
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-10 09:44:44 +0300

    Add search input to header component and adjust user button styling

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import "./Header.css";
 import logo from "../../assets/logo3.jpg";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
 
 function Header() {
   return (
     <div className="header-main">
       <img src={logo} alt="" />
       <div className="header-content">
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
-        <button className="header-button">Kategori 1</button>
+        <div className="header-search">
+          <input type="text" className="search-input" placeholder="Ara..." />
+          <button>
+            {" "}
+            <CiSearch />{" "}
+          </button>
+        </div>
+        <button className="header-button">Nano Cam Kesme Makinesi</button>
+        <button className="header-button">Akıllı Saat</button>
+        <button className="header-button"> Arka Kaplama</button>
+        <button className="header-button">Aksesuar</button>
+        <button className="header-button">Rova Nano</button>
+        <button className="header-button">Tüm Kategoriler</button>
       </div>
       <div className="header-user">
         <button className="user-button">
-          {" "}
           <FaRegCircleUser />
         </button>
       </div>
