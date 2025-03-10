31cf23673ad3d009dbf204f8bac65a0b15514ce8
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-10 09:30:39 +0300

    Add user icon button to header component and style adjustments

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../assets/logo3.jpg";
+import { FaRegCircleUser } from "react-icons/fa6";
 
 function Header() {
   return (
@@ -14,6 +15,12 @@ function Header() {
         <button className="header-button">Kategori 1</button>
         <button className="header-button">Kategori 1</button>
         <button className="header-button">Kategori 1</button>
+      </div>
+      <div className="header-user">
+        <button className="user-button">
+          {" "}
+          <FaRegCircleUser />
+        </button>
       </div>
     </div>
   );
