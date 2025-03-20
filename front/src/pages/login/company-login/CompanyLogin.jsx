8534818aadd8d4b2e0610d6381ef110798d34d65
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-20 23:44:24 +0300

    Add CompanyLogin and UserLogin components with basic structure and styles

---
+++ front/src/pages/login/company-login/CompanyLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CompanyLogin.css";
+function CompanyLogin() {
+  return (
+    <div className="company-login-main">
+      <div className="company-login-content"></div>
+    </div>
+  );
+}
+
+export default CompanyLogin;
---
+++ front/src/pages/login/user-login/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./UserLogin.css";
+function UserLogin() {
+  return (
+    <div className="user-login-main">
+      <div className="user-login-content"></div>
+    </div>
+  );
+}
+
+export default UserLogin;
