dc5b33c1681df1cccf8a9afcf77e2e5566a2a9f7
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-20 19:58:55 +0300

    Add click navigation to ProductCard image for improved user interaction

--- front/src/components/card/productcard/ProductCard.jsx
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { CiSquarePlus, CiSquareMinus } from "react-icons/ci";
 import { BsCart2 } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa";
-
+import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
 import urun from "../../../assets/urun.png";
 function ProductCard() {
+  const navigate = useNavigate();
+  const navigateTo = (navigationRoute) => {
+    navigate(navigationRoute);
+  };
   return (
     <div className="product-card-main">
       <div className="product-card-content">
-        <img src={urun} alt="" />
+        <img
+          onClick={() => navigateTo("/productinformation")}
+          src={urun}
+          alt=""
+        />
         <div className="product-card-text">
           <div className="product-card-text-row">
             <span className="product-card-title">
