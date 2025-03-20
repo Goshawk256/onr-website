35726f2fc9cc3aefe5396bf8c128f41eb77201aa
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-20 16:17:38 +0300

    Add hover effect and styling to product card component for enhanced interactivity

--- front/src/components/card/productcard/ProductCard.jsx
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -4,7 +4,18 @@ import urun from "../../../assets/urun.png";
 function ProductCard() {
   return (
     <div className="product-card-main">
-      <div className="product-card-content">selam bu bir ürün.</div>
+      <div className="product-card-content">
+        <div className="card-container">
+          <div className="card">
+            <div className="front-content">
+              <p>Hover me</p>
+            </div>
+            <div className="content">
+              <p className="heading">Card Hover</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
