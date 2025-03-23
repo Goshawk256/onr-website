52d4fecd83070581d712aa05ed89cb1613e4b0e1
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-23 03:37:58 +0300

    Update ProductCard component: replace placeholder images with actual images and change hover to click interactions for shoe selection

--- front/src/components/card/productcard/ProductCard.jsx
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
-import shoe1 from "../../../assets/shoes1.png";
-import shoe2 from "../../../assets/shoes2.png";
-import shoe3 from "../../../assets/shoes3.png";
+import shoe1 from "../../../assets/1.png";
+import shoe2 from "../../../assets/2.png";
+import shoe3 from "../../../assets/3.png";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { FaRegEye } from "react-icons/fa";
 
 const ProductCard = () => {
   const [selectedShoe, setSelectedShoe] = useState(shoe1);
+  const [animate, setAnimate] = useState(false);
   const navigate = useNavigate();
+
   const navigateToProduct = () => {
     navigate("/productinformation");
   };
+
+  const handleShoeChange = (shoe) => {
+    setAnimate(true);
+    setSelectedShoe(shoe);
+
+    setTimeout(() => {
+      setAnimate(false);
+    }, 1000);
+  };
+
   return (
     <div className="product-container">
       <div className="thumb">
         <ul>
-          <li onMouseOver={() => setSelectedShoe(shoe1)}>
+          <li onClick={() => handleShoeChange(shoe1)}>
             <img src={shoe1} alt="Shoe 1" />
           </li>
-          <li onMouseOver={() => setSelectedShoe(shoe2)}>
+          <li onClick={() => handleShoeChange(shoe2)}>
             <img src={shoe2} alt="Shoe 2" />
           </li>
-          <li onMouseOver={() => setSelectedShoe(shoe3)}>
+          <li onClick={() => handleShoeChange(shoe3)}>
             <img src={shoe3} alt="Shoe 3" />
           </li>
         </ul>
       </div>
       <div className="imgBox">
         <h2>Nike Air Zoom</h2>
-        <img src={selectedShoe} alt="Selected Shoe" className="shoes" />
+        <img
+          onClick={() => handleShoeChange(selectedShoe)}
+          src={selectedShoe}
+          alt="Selected Shoe"
+          className={`shoes ${animate ? "animate" : ""}`}
+        />
         <ul className="sizes">
           <span>Beden: </span>
           <li>41</li>
