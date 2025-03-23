dd681a546537bd55ba4e46dc572c802aad7289c9
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-23 03:34:51 +0300

    Update product images in ProductCard component for better asset management

--- front/src/components/card/productcard/ProductCard.jsx
+++ front/src/components/card/productcard/ProductCard.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
-import shoe1 from "../../../assets/urunonr.png";
-import shoe2 from "../../../assets/urunonr.png";
-import shoe3 from "../../../assets/urunonr.png";
+import shoe1 from "../../../assets/1.png";
+import shoe2 from "../../../assets/2.png";
+import shoe3 from "../../../assets/3.png";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { FaRegEye } from "react-icons/fa";
 
